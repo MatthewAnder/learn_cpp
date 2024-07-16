2cc67afec393b64d2852c430ac642f947744798a
Matthew <mharyant@students.cs.ubc.ca>
2024-07-15 17:28:57 -0700

    initializing an empty window

--- Intro/Intro/main.cpp
+++ Intro/Intro/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello World!";
+	std::cout << "Hello World!" << "\n";
+	// prints whatever on the right side to the console
+	// there is a variety ways of using cout
+	std::cout << "Hi" << "\t" << 2 << "\n";
+	// These are outputs
+
+	// Inputs
+	int8_t num{};
+	std::cout << "Input an 8 bit integer: ";
+	std::cin >> num;
+	std::cout << num;
+
+	
+
+
+
 	return 0;
 }
